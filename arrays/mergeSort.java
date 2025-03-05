4ad5847aae03a06d633fa613f12cbc053f30ab65
Ashwinigadad <ashwinigadad3@gmail.com>
2025-03-05 10:28:02 +0530

    sort

---
+++ arrays/mergeSort.java
@@ -0,0 +1,23 @@
+class mergeSort{
+    public static void ins(int a[]){
+        int n=a.length;
+        for(int i=1;i<n;i++){
+            int key=a[i];
+            int j=i-1;
+            while(j>=0 && a[j]>key){
+                a[j+1]=a[j];
+                j--;
+            }
+            a[j+1]=key;
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+
+    public static void main(String [] args){
+        int a[]={2,-3,4,8,9,1};
+        ins(a);
+    }
+}
